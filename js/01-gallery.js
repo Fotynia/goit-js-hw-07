e6bdcddec9fa2dbda6e253013034a4659840b25e
Svitlana Dyachok <korneyswet@gmail.com>
2023-03-06 00:59:36 +0200

    gallery

---
+++ js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+const gallery = document.querySelector('.gallery');
+const cardsMarkup = createImgCardsMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', cardsMarkup);
+gallery.addEventListener('click', onPictureClick);
+function createImgCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+       <a class="gallery__link" href="${original}">
+         <img
+           class="gallery__image"
+           src="${preview}"
+           data-source="${original}"
+           alt="${description}"
+          />
+        </a>
+      </div>
+      `;
+    })
+    .join('');
+}
+function onPictureClick(evt) {
+  evt.preventDefault();
+  const galleryImage = evt.target.classList.contains('gallery__image');
+  if (!galleryImage) {
+    return;
+  }
+  gallery.addEventListener('click', onModal);
+}
+function onModal(evt) {
+  const instance = basicLightbox.create(
+    `<img src="${evt.target.dataset.source}"/>`,
+    {
+      onShow: _instance => {
+        gallery.addEventListener('keydown', onEscBtn);
+      },
+      onClose: _instance => {
+        gallery.removeEventListener('keydown', onEscBtn);
+      },
+    },
+  );
+  instance.show();
+  function onEscBtn(evt) {
+    if (evt.code === 'Escape') {
+    }
+    instance.close();
+  }
+}
